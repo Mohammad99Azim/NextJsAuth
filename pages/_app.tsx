f20126933ca40a5f6ccda2554874f76b374c279e
IvyJ <inbaejj117@gmail.com>
2021-05-07 13:14:14 +0900

    feat: add scss files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
 import 'antd/dist/antd.css'
+import '../public/main.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
